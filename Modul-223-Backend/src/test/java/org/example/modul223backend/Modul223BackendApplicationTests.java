399a2116870fc66dd4b6933269ed71d17f4414bc
Zen Zalapski <zenzz@hotmail.ch>
2024-12-21 10:47:06 +0100

    Some Changes

---
+++ Modul-223-Backend/src/test/java/org/example/modul223backend/Modul223BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.modul223backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modul223BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
